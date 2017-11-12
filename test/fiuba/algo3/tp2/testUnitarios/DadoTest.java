b14f71276052ccb71edfc012d8e98ec91e0046f5
matiaspiersantolini <matias.piersantolini@gmail.com>
2017-11-12 17:54:20 -0300

    La clase Dados paso de ser Dado, cambiando su implementacion por la funcionalidad de un solo dado, en vez de los dos anteriores.

--- test/fiuba/algo3/tp2/testUnitarios/DadoTest.java
+++ test/fiuba/algo3/tp2/testUnitarios/DadoTest.java
@@ -6,24 +6,24 @@ import org.junit.Test;
 
 import fiuba.algo3.tp2.*;
 
-public class DadosTest {
+public class DadoTest {
 
 	private static final int MINIMO = 1;
-	private static final int MAXIMO = 12;
+	private static final int MAXIMO = 6;
 	
 	@Test
-	public void test01CrearDadosNoEsNulo() {
-		Dados unosDados = new Dados();
-		assertNotNull(unosDados);
+	public void test01CrearDadoNoEsNulo() {
+		Dado unDado = new Dado();
+		assertNotNull(unDado);
 	}
 	
 	@Test
-	public void test02TirarLosDadosEstaDentroDeLoRangosPermitidos() {
-		Dados unosDados = new Dados();
+	public void test02TirarElDadoEstaDentroDelRangoPermitido() {
+		Dado unDado = new Dado();
 		boolean verificado = false;
-		int numero = unosDados.tirar();
+		int numero = unDado.tirar();
 		if (numero >= MINIMO && numero <= MAXIMO) verificado = true;
 		assertTrue(verificado);
 	}
-
+	
 }
